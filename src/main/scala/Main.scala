6bed7ba85ebcc4d3e18d707926015fdbb8dee14f
Ivan.Shchekoldin <darthmantis@yandex.ru>
2019-08-18 20:36:49 +0300

    remove code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,7 @@
 import better.files.File
-import parse.VideoFileNameParser
 
 object Main {
   def main(args: Array[String]): Unit = {
     val startDirectory = File.apply(args(0))
-    val childrens = startDirectory.children
-    val filenames = childrens
-      .map(t => t.path.getFileName)
-      .map(t => t.toFile)
-      .map(t => t.toString)
-      .map(f => f.split("\\.").apply(0))
-    val names = filenames.map(t => VideoFileNameParser.parse(t))
-    names.foreach(t => println(t))
   }
 }
