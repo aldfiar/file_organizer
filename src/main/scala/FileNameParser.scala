f553ae97a59739ef80a4109454b4275ed13ada20
Ivan Shchekoldin <iv.sheckoldin@yandex.ru>
2019-08-18 11:22:08 +0300

    change logic

--- src/main/scala/FileNameParser.scala
+++ src/main/scala/FileNameParser.scala
@@ -1,57 +1,33 @@
+
+
 trait FileNameParser {
+  def parse(filename: String, template: Array[String] => String): String
   def parse(filename: String): String
 }
 
 object HentaiNameParser extends FileNameParser {
-  val symbols = Seq(('(', ')'), ('[', ']'), ('{', '}'))
-  val symbolsToRemove = Seq('_', '-', '~')
+  val bracketRegEx = "[\\[,(,{].*?[\\],),}]".r
+  val symbolsToRemove = Seq("_", "-", "~", "")
+
+
+  override def parse(filename: String, template: Array[String] => String): String = {
+    val nameWithoutBrackets = bracketFinder(filename)
+    val title = nameWithoutBrackets.split("[  _|-]").filter(t => !symbolsToRemove.contains(t))
+    template.apply(title)
+  }
+
+  def bracketFinder(title: String): String = {
+    var tempName = title
+    val removed = bracketRegEx.findAllIn(title).toList
+    for (el <- removed)
+      tempName = tempName.replace(el, "")
+    tempName
+  }
 
   override def parse(filename: String): String = {
-    val splittedName = filename.split("\\.")
-    if (splittedName.length > 2) {
-      throw new IllegalArgumentException("File:%s contains multiple dots".format(filename))
-    }
-    val extension = splittedName(1)
-    var name = splittedName(0)
-    for ((startSymbol, endSymbol) <- symbols)
-      name = bracketFinder(name, startSymbol, endSymbol)
-
-    val withoutSymbols = removeLeadingAndTrailing(name)
-    val newName = withoutSymbols.trim
-    newName.concat(".").concat(extension)
+    val nameWithoutBrackets = bracketFinder(filename)
+    val title = nameWithoutBrackets.split("[  _|-]").filter(t => !symbolsToRemove.contains(t))
+    title.mkString(" ")
   }
-
-  def removeLeadingAndTrailing(value: String): String = {
-    var startPos = 0
-    var endPos = value.length - 1
-    var findPos = false
-    while (!findPos) {
-      if (!symbolsToRemove.contains(value(startPos))) {
-        findPos = true
-      }
-      else
-        startPos += 1
-    }
-    findPos = false
-    while (!findPos) {
-      if (!symbolsToRemove.contains(value(endPos))) {
-        findPos = true
-      }
-      else
-        endPos -= 1
-    }
-    value.substring(startPos, endPos + 1)
-  }
-
-  def bracketFinder(stringValue: String, startSymbol: Char, endSymbol: Char): String = stringValue.contains(startSymbol) && stringValue.contains(endSymbol) match {
-    case true =>
-      val startIndex = stringValue.indexOf(startSymbol)
-      val endIndex = stringValue.indexOf(endSymbol)
-      val removedPart = stringValue.substring(startIndex, endIndex + 1)
-      val replaced = stringValue.replace(removedPart, "")
-      bracketFinder(replaced, startSymbol, endSymbol)
-    case false => stringValue
-  }
-
 }
 
--- src/test/scala/HentaiNameParserTest.scala
+++ src/test/scala/HentaiNameParserTest.scala
@@ -1,25 +1,35 @@
 import org.scalatest.FunSuite
 
 class HentaiNameParserTest extends FunSuite {
+
   test("Parse simple file") {
-    val fileName = "Learn_Scala_faggot!_3.mp4"
-    val parsed = HentaiNameParser.parse(fileName)
-    assert(parsed == fileName)
+    val fileName = "Learn_Scala_faggot!_3"
+    val parsed = HentaiNameParser.parse(fileName, template)
+    assertResult("Learn Scala faggot! - 3")(parsed)
 
   }
+
   test("Parse file name with unnecessary symbols") {
-    val fileName = "Baka_User_XX_Use_Sudo_01_[F2A5991E].mkv"
-    val parsed = HentaiNameParser.parse(fileName)
-    assert(parsed == "Baka_User_XX_Use_Sudo_01.mkv")
+    val fileName = "Baka_User_XX_Use_Sudo_01_[F2A5991E]"
+    val parsed = HentaiNameParser.parse(fileName, template)
+    assertResult("Baka User XX Use Sudo - 01")(parsed)
   }
   test("Parse name with leading and trailing unnecessary symbols") {
-    val fileName = "[Vasya-Raws]_Oh_my_Stack_overflow_3_The_Animation_-_02_[6031D3AD].mkv"
-    val parsed = HentaiNameParser.parse(fileName)
-    assert(parsed == "Oh_my_Stack_overflow_3_The_Animation_-_02.mkv")
+    val fileName = "[Vasya-Raws]_Oh_my_Stack_overflow_The_Animation_-_02_[6031D3AD]"
+    val parsed = HentaiNameParser.parse(fileName, template)
+    assertResult("Oh my Stack overflow The Animation - 02")(parsed)
   }
   test("Parse name with leading unnecessary symbol and bracket without underscore") {
-    val fileName = "[AWS] Ricardo Milos dance OVA -02- (DVDRip 1280x720 h264 ac3).mkv"
-    val parsed = HentaiNameParser.parse(fileName)
-    assert(parsed == "Ricardo Milos dance OVA -02-.mkv")
+    val fileName = "[AWS] Ricardo Milos dance OVA -02- (DVDRip 1280x720 h264 ac3)"
+    val parsed = HentaiNameParser.parse(fileName, template)
+    assertResult("Ricardo Milos dance OVA - 02")(parsed)
   }
+
+  def template(elements: Array[String]): String = {
+    val numbers = elements.filter(t => t.exists(_.isDigit))
+    val withoutNumbers = elements.diff(numbers)
+    val lastNumber = numbers.lastOption.map(t => " - %s".format(t)).getOrElse("")
+    withoutNumbers.mkString(" ").concat(lastNumber)
+  }
+
 }
